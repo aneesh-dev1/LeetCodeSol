9b723e735fc800ed6ef8008e15cfd81301101427
Aneesh Kumar <71940535+aneesh-dev1@users.noreply.github.com>
2022-09-13 23:59:06 +0530

    Time: 30 ms (21.20%), Space: 14 MB (38.65%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.cpp
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
     bool validUtf8(vector<int>& data) {
-        int bytes_rem = 0;
+        int bytes_remaining = 0;
         for(auto x:data){
-            string s = "";
+            string s= "" ; 
             for(int i=0;i<8;i++){
-                s += (x%2==0 ? "0" : "1");
-                x = x/2;
+                s += (x%2==0?'0':'1');
+                x/=2;
             }
             reverse(s.begin(),s.end());
-            if(bytes_rem == 0){
-                if(s[0] == '0')
-                    continue;
+            if(bytes_remaining == 0){
+                if(s[0] == '0') continue;
                 int i=0;
-                for(i=0;i<8;i++){
+                for(;i<8;i++){
                     if(s[i] == '0'){
                         if(i == 1) return false;
-                        bytes_rem = i-1;
+                        bytes_remaining = i-1;
                         break;
                     }
                 }
-                if(i==8 || bytes_rem>=4){
-                    return false;
-                }
+                if(i==8 || bytes_remaining>=4) return false;
             }
-                else if(bytes_rem > 0){
-                    if(s[0] != '1' || s[1] != '0')
-                        return false;
-                    bytes_rem--;
-                }
+            else if(bytes_remaining>0){
+                if(s[0] != '1' || s[1] != '0') return false;
+                bytes_remaining--;
+            }
         }
-        return (bytes_rem == 0);
+        return bytes_remaining == 0;
     }
 };
