a380d49d95bf3f8bec1dc06d73e23ceb11fa9ac7
Aneesh Kumar <71940535+aneesh-dev1@users.noreply.github.com>
2022-09-15 23:41:45 +0530

    Time: 28 ms (42.82%), Space: 13.9 MB (76.17%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.cpp
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     bool validUtf8(vector<int>& data) {
-        int bytes_remaining = 0;
+        int n = 0;
         for(auto x:data){
-            string s= "" ; 
+            string s = "";
             for(int i=0;i<8;i++){
-                s += (x%2==0?'0':'1');
+                s += (x%2==0)?'0':'1';
                 x/=2;
             }
             reverse(s.begin(),s.end());
-            if(bytes_remaining == 0){
+            if( n == 0 ){
                 if(s[0] == '0') continue;
                 int i=0;
                 for(;i<8;i++){
                     if(s[i] == '0'){
                         if(i == 1) return false;
-                        bytes_remaining = i-1;
+                        n = i-1;
                         break;
                     }
                 }
-                if(i==8 || bytes_remaining>=4) return false;
+                if(i == 8 || n>=4) return false;
             }
-            else if(bytes_remaining>0){
-                if(s[0] != '1' || s[1] != '0') return false;
-                bytes_remaining--;
+            else if(n>0){
+                if(s[0] != '1' || s[1] != '0')
+                    return false;
+                n--;
             }
         }
-        return bytes_remaining == 0;
+        return n == 0;
     }
 };
